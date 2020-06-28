ac75ab4a3be84c96d2f0b839838ae66e8616faf6
Hadar Bejerano <hadar@cloudinary.com>
2020-06-28 12:33:07 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 750,
+    height: 550,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      devTools: false,
+    },
+  });
+  win.loadURL(`file://${__dirname}/index.html`);
+}
+
+app.on("ready", createWindow);
+app.on("will-navigate", function (event) {
+  event.preventDefault();
+});
+
+ipcMain.handle("open-file-dialog", async (event) => {
+  const buf = await dialog.showOpenDialog(win, {
+    properties: ["openDirectory"],
+  });
+  return buf.filePaths;
+});
+
+ipcMain.handle("app-name", (event) => {
+  return app.getName();
+});
