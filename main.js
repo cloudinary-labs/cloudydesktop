e186666ceb00f5e3f6bfc8a34ad7a4faf8b8bc1f
hadarbj <hadarbj@gmail.com>
2020-07-30 08:56:34 +0300

    Mac only, hide on close, stop sync dock menu

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const { app, Menu, BrowserWindow, ipcMain, dialog } = require("electron");
 
-let win;
+let win = null;
+var syncEnabled = true;
+
+//Mac only stop sync option in dock menu
+const dockMenu = Menu.buildFromTemplate([
+  {
+    label: "Stop Sync",
+    click() {
+      console.log("Stop Sync");
+      if (win) {
+        syncEnabled = false;
+        win.close();
+        syncEnabled = true;
+        win = null;
+      }
+    }
+  }
+]);
+
+function startApp() {
+  createWindow();
+  app.dock.setMenu(dockMenu);
+}
 
 function createWindow() {
   win = new BrowserWindow({
@@ -14,12 +36,28 @@ function createWindow() {
   });
   //  win.webContents.openDevTools();
   win.loadURL(`file://${__dirname}/index.html`);
+  // On Mac window close hide it and close it on quit
+  win.on('close', (event) => {
+    if (!app.quitting && syncEnabled) {
+      event.preventDefault();
+      win.hide();
+    }
+  })
 }
 
-app.on("ready", createWindow);
+app.on("ready", startApp);
 app.on("will-navigate", function (event) {
   event.preventDefault();
 });
+// On Mac window close hide it and close it on quitn
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+app.on('activate', () => { win ? win.show() : createWindow()})
+
+app.on('before-quit', () => app.quitting = true)
 
 ipcMain.handle("open-file-dialog", async (event) => {
   const buf = await dialog.showOpenDialog(win, {
