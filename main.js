bdbac8eb4a6a1e5541918edbb0e2f813ee488e4c
hadarbj <hadarbj@gmail.com>
2020-07-31 13:38:54 +0300

    Add Windows only thumbnail toolbar

--- main.js
+++ main.js
@@ -13,10 +13,33 @@ const dockMenu = Menu.buildFromTemplate([
   }
 ]);
 
+function setThumbar() {
+  win.setThumbarButtons([
+    {
+      tooltip: "button1",
+      icon: path.join(__dirname, "button1.png"),
+      click() {
+        console.log("button1 clicked");
+      },
+    },
+    {
+      tooltip: "button2",
+      icon: path.join(__dirname, "button2.png"),
+      flags: ["enabled", "dismissonclick"],
+      click() {
+        console.log("button2 clicked.");
+      },
+    },
+  ]);
+}
+
 function startApp() {
   createWindow();
   if (process.platform === "darwin") {
     app.dock.setMenu(dockMenu);
+  }
+  if (process.platform === "win") {
+    setThumbar();
   }
 }
 
